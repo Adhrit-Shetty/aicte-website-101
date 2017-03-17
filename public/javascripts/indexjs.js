364bc850c8a27d90e76f61a02b8e9f81c4531319
ashniu123 <nisheet1.sinvhal@gmail.com>
2017-03-17 23:04:39 +0530

    Basic Idea

--- public/javascripts/indexjs.js
+++ public/javascripts/indexjs.js
@@ -1,3 +1,42 @@
-$(document).on('click', '.yamm .dropdown-menu', function(e) {
-    e.stopPropagation()
-})
+//Internet Explorer 10 in Windows Phone 8
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            '@-ms-viewport{width:auto!important}'
+        )
+    );
+    document.head.appendChild(msViewportStyle)
+}
+//Android stock Browser
+$(function () {
+    var nua = navigator.userAgent
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+    if (isAndroid) {
+        $('select.form-control').removeClass('form-control').css('width', '100%')
+    }
+});
+
+//Enable bootstrap's material Design
+$('body').bootstrapMaterialDesign();
+
+//Group Tree
+!function ($) {
+
+    // Le left-menu sign
+    /* for older jquery version
+     $('#left ul.nav li.parent > a > span.sign').click(function () {
+     $(this).find('i:first').toggleClass("icon-minus");
+     }); */
+
+    $(document).on("click","#left ul.nav li.parent > a > span.sign", function(){
+        $(this).find('i:first').toggleClass("icon-minus");
+    });
+
+    // Open Le current menu
+    $("#left ul.nav li.parent.active > a > span.sign").find('i:first').addClass("icon-minus");
+    $("#left ul.nav li.current").parents('ul.children').addClass("in");
+
+}(window.jQuery);
+
+/********************/
