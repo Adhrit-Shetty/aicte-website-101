298894e40beb5c55ed91d6c70f08688de15425ce
ashniu123 <nisheet1.sinvhal@gmail.com>
2017-03-22 11:56:40 +0530

    New Navbar

--- public/javascripts/indexjs.js
+++ public/javascripts/indexjs.js
@@ -53,8 +53,42 @@ $('#fullNav').affix({
 
 $('#fullNav').on('affix.bs.affix', function () {
     $('main').addClass('pushDown');
+    $('#miniNav').removeClass('not-top');
 });
 
 $('#fullNav').on('affix-top.bs.affix', function () {
     $('main').removeClass('pushDown');
+    $('#miniNav').addClass('not-top');
 });
+
+$(document).ready(function () {
+    var sideslider = $('[data-toggle=collapse-side]');
+    var sel = sideslider.attr('data-target');
+    var sel2 = sideslider.attr('data-target-2');
+    sideslider.click(function (event) {
+        $(sel).toggleClass('in');
+        $(sel2).toggleClass('out');
+        $('#fullNav').toggleClass('navbar-fixed-top');
+        $('#fullNav').toggleClass('affix-top');
+        $('#miniNav').toggleClass('not-top');
+        $('body').toggleClass('noscroll opaque');
+    });
+
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $("body").hasClass("noscroll opaque");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("#collapse-menu").click();
+        }
+    });
+});
+
+$('#collapse-menu').click(function () {
+    $('#bar1').toggleClass('top-bar');
+    $('#bar2').toggleClass('middle-bar');
+    $('#bar3').toggleClass('bottom-bar');
+});
+
+$('')
+
+/********************/
