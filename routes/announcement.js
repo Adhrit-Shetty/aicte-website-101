085e1187c111c430f628e772175e4deeb2fba5a2
Akshat Shetty <akshatshetty2908@gmail.com>
2017-03-23 20:45:43 +0530

    Merged Home into Master (#7)
    * Updated Styling
    
    * Updated Styling
    
    * Added many pages
    
    * Fixed many sideNav issues
    
    * fixed height for boxes
    
    * added material styles
    
    * added material styles #2
    
    * Added all pages
    
    * fixed most stuff and added functionality
    
    * Resolved Merge Conflict 5pm Kevin's House
    
    * Created announcement.html
    
    * Created announcement_list.html
    
    * Created announcement.js
    
    * Created announcement.js
    
    * Created announcement_list.js router
    
    * Updated app.js and update_year.js

---
+++ routes/announcement.js
@@ -0,0 +1,33 @@
+//========================IMPORTS=======================================
+var express = require('express');
+var app = express();
+var http = require('http');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+var Announcement = require('../models/announcement');
+
+var A = express.Router();
+A.use(bodyParser.json());
+A.use(bodyParser.urlencoded({extended : true}));
+//====================================================================================
+//===========================IMPLEMENTATION===========================================
+app.use(morgan('dev'));
+//====================================================================================
+//===========================ROUTING==================================================
+A.route('/')
+    .post(function(request,response,next) {
+        console.log(request.body);
+        Announcement.create(request.body,function(err,data){
+            if(err)
+                response.json(err);
+            else
+            {
+                response.json(data);
+            }
+        });
+    })
+
+//====================================================================================
+module.exports=A;
