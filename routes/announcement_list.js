ec6825bb0ce409bd3a3addf69dcaf2a2a837856b
Akshat Shetty <akshatshetty2908@gmail.com>
2017-03-29 02:00:39 +0530

    Corrected announcement_list.js

--- routes/announcement_list.js
+++ routes/announcement_list.js
@@ -1,23 +1,19 @@
-//========================IMPORTS=======================================
+//========================IMPORTS===================================================
 var express = require('express');
 var app = express();
 var http = require('http');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
-var url = 'mongodb://localhost:27017/Aicte101';
 var Announcement = require('../models/announcement');
-
-var A = express.Router();
-A.use(bodyParser.json());
-A.use(bodyParser.urlencoded({extended : true}));
+var router = express.Router();
 //====================================================================================
 //===========================IMPLEMENTATION===========================================
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended : true}));
 app.use(morgan('dev'));
 //====================================================================================
 //===========================ROUTING==================================================
-A.route('/')
+router.route('/')
     .post(function(request,response){
         Announcement.find({},{"_id" : 0, "date" :1,"name" : 1,"href" : 1},{sort : {"date" :-1}},function(err,data){
             console.log("INSIDE!!!");
@@ -28,5 +24,6 @@ A.route('/')
                 response.json(data);
             }
         });
-    })//====================================================================================
-module.exports=A;
+    });
+//====================================================================================
+module.exports=router;
