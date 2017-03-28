6da6469ae6bb1b1c0a0b574f4e14ebfc1445adcf
Akshat Shetty <akshatshetty2908@gmail.com>
2017-03-29 02:02:31 +0530

    Added aggregate functionality to dashboard.

--- routes/querydb.js
+++ routes/querydb.js
@@ -4,19 +4,15 @@ var app = express();
 var http = require('http');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
-var async = require('async');
 var Institute = require('../models/institution');
 var Year = require('../models/year');
 var Verify = require('./verify');
 var router = express.Router();
-var out = [];
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended : true}));
+var _ = require('underscore');
 //====================================================================================
 //===========================IMPLEMENTATION===========================================
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended : true}));
 app.use(morgan('dev'));
 //====================================================================================
 //===========================ROUTING==================================================
@@ -24,28 +20,47 @@ router.post('/',function(request,response) {
     console.log(request.body);
     Institute.find(request.body.institute,function (err, idata) {
         if (err)
-            response.json(err);
+            response.json(err.status);
         else if (idata == undefined)
             response.json("No such entry!");
         else {
-            console.log(idata);
+            console.log("i"+idata);
+            Year.aggregate({$ :{"y" : 1}});
             var query = {
                 $and:[request.body.year,{'instituteid':{$in :idata}}]};
             Year.find(query,{"_id" : 0,"y" : 1,"intake" : 1,"passed" : 1,"placed" : 1,"enrolled" : 1,"instituteid" : 1})
                 .populate('instituteid',{'name' :1 ,"_id" : 0})
                 .sort({"instituteid": -1,"y" : -1})
-                .exec(function (err, ydata) {
+                .exec(function (err, ydata){
                 if (err)
-                    response.json(err);
+                   response.json(err);
                 else if (ydata == undefined)
                     response.json("No such entry!");
                 else {
-
-                      response.json(ydata);
+                    var tmp = _.groupBy(ydata,"y",function(d){
+                        return d;
+                    });
+                    var out = _(tmp).map(function(g, key) {
+                        return {
+                            y: key,
+                            intake: _(g).reduce(function (m, x) {
+                                return m + x.intake;
+                            }, 0),
+                            enrolled: _(g).reduce(function (m, x) {
+                                return m + x.enrolled;
+                            }, 0),
+                            passed: _(g).reduce(function (m, x) {
+                                return m + x.passed;
+                            }, 0),
+                            placed: _(g).reduce(function (m, x) {
+                                return m + x.placed;
+                            }, 0)
+                        }
+                    });
+                    response.json(out);
                 }
             });
         }
-
     });
 });
 //====================================================================================
