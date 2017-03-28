97f97d2d046d7faf13921b19c4263c5a1c245f26
Akshat Shetty <akshatshetty2908@gmail.com>
2017-03-29 02:05:38 +0530

    Cleaned year.js

--- routes/year.js
+++ routes/year.js
@@ -4,56 +4,52 @@ var app = express();
 var http = require('http');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
-var url = 'mongodb://localhost:27017/Aicte101';
 var Year = require('../models/year');
 var Institute = require('../models/institution');
-
 var router = express.Router();
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended : true}));
 //====================================================================================
 //===========================IMPLEMENTATION===========================================
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended : true}));
 app.use(morgan('dev'));
 //====================================================================================
 //===========================ROUTING==================================================
 router.route('/')
-    .post(function(request,response,next){
+    .post(function(request,response){
         console.log(request.body);
-    Institute.find({'name': request.body.name},{'_id' : 1},function(err, data){
-        if(err)
-            response.json(err);
-        else
-        {
-            console.log(data);
-            Year.create({'y':request.body.y,'intake' : request.body.intake,'enrolled' : request.body.enrolled,
-            'passed' : request.body.passed,'placed' : request.body.placed,
-            'instituteid' : data[0]},function (error,new_data){
-                if(err)
-                    response.json(err);
-                else
-                {
-                    console.log(new_data);
-                    Institute.findById(data[0],function(err,inst_data){
-                        if(err)
-                            response.json(err);
-                        else
-                        {
-                            inst_data.year.push(new_data._id);
-                            inst_data.save(function(err,new_inst_data){
-                                if(err)
-                                    response.json(err);
-                                else
-                                {
-                                    response.json([new_inst_data,new_data]);
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }
+        Institute.find({'name': request.body.name},{'_id' : 1},function(err, data){
+            if(err)
+                response.json(err);
+            else
+            {
+                console.log(data);
+                Year.create({'y':request.body.y,'intake' : request.body.intake,'enrolled' : request.body.enrolled,
+                'passed' : request.body.passed,'placed' : request.body.placed,
+                'instituteid' : data[0]},function (error,new_data){
+                    if(err)
+                        response.json(err);
+                    else
+                    {
+                        console.log(new_data);
+                        Institute.findById(data[0],function(err,inst_data){
+                            if(err)
+                                response.json(err);
+                            else
+                            {
+                                inst_data.year.push(new_data._id);
+                                inst_data.save(function(err,new_inst_data){
+                                    if(err)
+                                        response.json(err);
+                                    else
+                                    {
+                                        response.json([new_inst_data,new_data]);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
         }).limit(1);
     });
 //====================================================================================
