a3644cb447d258e25a4a9cc5ab6211a3797215d1
Akshat Shetty <akshatshetty2908@gmail.com>
2017-03-29 02:01:54 +0530

    Corrected institute.js

--- routes/institute.js
+++ routes/institute.js
@@ -1,32 +1,26 @@
-//========================IMPORTS=======================================
+//========================IMPORTS====================================================
 var express = require('express');
 var app = express();
 var http = require('http');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
-var url = 'mongodb://localhost:27017/Aicte101';
 var Inst = require('../models/institution');
-
 var institute = express.Router();
-institute.use(bodyParser.json());
-institute.use(bodyParser.urlencoded({extended : true}));
 //====================================================================================
 //===========================IMPLEMENTATION===========================================
+institute.use(bodyParser.json());
+institute.use(bodyParser.urlencoded({extended : true}));
 app.use(morgan('dev'));
 //====================================================================================
 //===========================ROUTING==================================================
 institute.route('/')
-    .post(function(request,response,next){
+    .post(function(request,response){
         console.log(request.body);
         Inst.create(request.body, function (err, data) {
             if (err) throw err;
             console.log('institute added!');
             response.json(data);
-            // response.redirect('/');
         });
-
     });
 //====================================================================================
 module.exports=institute;
